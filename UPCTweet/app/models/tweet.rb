c93694c06eb992d976f64a2986974f24fc049d27
Daniel Subauste Oliden <daalsuol@gmail.com>
2014-10-02 12:23:36 -0500

    Update tweet.rb

--- UPCTweet/app/models/tweet.rb
+++ UPCTweet/app/models/tweet.rb
@@ -2,10 +2,10 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 class Tweet < ActiveRecord::Base
   def initialize(params={})    
     @client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "UgV1xvQTDwIsWTxaGR1RLIkdt"
-    config.consumer_secret     = "s0XpyLDY2RDnGBMET08TcmaU0MG7RiFAyWcrtTSSgMpFrEBhGz"
-    config.access_token        = "344334932-aDCfHC98erUevFvEWex2jHMr4KjCVJ2RehR6X9gv"
-    config.access_token_secret = "4EHV1xLOtb00cOoNgyoD7NMnTH7zwlon27SqZhZ5McoxB"  
+    config.consumer_key        = "XXXXXXXXXXXXXXXXXXXXXx"
+    config.consumer_secret     = "XXXXXXXXXXXXXXXXXXXXXx"
+    config.access_token        = "XXXXXXXXXXXXXXXXXXXXXx-XXXXXXXXXXXXXXXXXXXXXx"
+    config.access_token_secret = "XXXXXXXXXXXXXXXXXXXXXx"  
     end
 	super
   end 
